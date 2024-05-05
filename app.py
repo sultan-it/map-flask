1ce5c36d0c35e017ba1cead7101f6ac7031c3a3c
sultan-it <157215085+sultan-it@users.noreply.github.com>
2024-05-05 21:08:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-# save this as app.py
-from flask import Flask
+from flask import Flask, redirect
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
-def map_link():
+def map_lin():
+    longitude = requests.get("https://Blynk.cloud/external/api/get?token=qIpqgPqk7Zpw7L6juManzZs7SujZSngS&V1").text
+    print(longitude)
+    latitude = requests.get("https://Blynk.cloud/external/api/get?token=qIpqgPqk7Zpw7L6juManzZs7SujZSngS&V2").text
 
-        return "Error fetching data"
-
-if __name__ == '_main_':
-
-    app.run()
+    map_url = redirect(f"https://www.google.com/maps/search/?api=1&query={longitude},{latitude}")
+    return map_url
